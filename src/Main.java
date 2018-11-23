f02b3a0028a6d692fd2c9828863785716a324208
aluelis <svetlintanyi@gmail.com>
2018-11-23 19:31:18 +0100

    0.743950

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<double[]> messageList = new ArrayList<>();
-        ArrayList<Double> valueList = new ArrayList<>();
-        ArrayList<double[]> testList = new ArrayList<>();
-        HashMap<Double, Double> valueMap = new HashMap<>();
+        ArrayList<int[]> messageList = new ArrayList<>();
+        ArrayList<Integer> valueList = new ArrayList<>();
+        ArrayList<int[]> testList = new ArrayList<>();
+        HashMap<Integer, Integer> positiveList = new HashMap<>();
+        HashMap<Integer, Integer> negativeList = new HashMap<>();
+        HashMap<Integer, Integer> wordCounter = new HashMap<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int lineCount = 0;
         String s;
@@ -21,53 +23,54 @@ public class Main {
                 if (lineCount <= 80000) {
                     if (!s.equals("")) {
                         messageList.add(Stream.of(s.split("\t"))
-                                .mapToDouble(Double::parseDouble)
+                                .mapToInt(Integer::parseInt)
                                 .toArray());
                     } else {
-                        messageList.add(new double[0]);
+                        messageList.add(new int[0]);
                     }
                 } else if (lineCount <= 160000) {
-                    valueList.add(Double.parseDouble(s));
+                    valueList.add(Integer.parseInt(s));
                 } else {
                     if (!s.equals("")) {
                         testList.add(Stream.of(s.split("\t"))
-                                .mapToDouble(Double::parseDouble)
+                                .mapToInt(Integer::parseInt)
                                 .toArray());
                     } else {
-                        testList.add(new double[0]);
+                        testList.add(new int[0]);
                     }
                 }
             }
 
             int counter = 0;
-            for (double[] list : messageList) {
-                for (double item : list) {
-                        if (valueList.get(counter) == 0.0) {
-                            valueMap.put(item, Math.max(valueMap.containsKey(item) ? valueMap.get(item) - 0.01 : 0.49, 0.0));
-                        } else {
-                            valueMap.put(item, Math.min(valueMap.containsKey(item) ? valueMap.get(item) + 0.01 : 0.51, 1.0));
-                        }
+            for (int[] list : messageList) {
+                for (int item : list) {
+                    if (valueList.get(counter) == 0) {
+                        //negativeList put in item and occurrence++
+                        negativeList.put(item, negativeList.containsKey(item) ? negativeList.get(item) + 1 : 1);
+                    } else {
+                        //positiveList put in item and occurrence++
+                        positiveList.put(item, positiveList.containsKey(item) ? positiveList.get(item) + 1 : 1);
+                    }
+                    wordCounter.put(item, 1);
                 }
                 counter++;
             }
 
-            for (double[] list : testList) {
-                double average = 0.0;
-                for (double item : list) {
-                    if (valueMap.get(item) != null) {
-                        average += valueMap.get(item);
-                    } else {
-                        average += 0.5;
-                    }
+            for (int[] list : testList) {
+                double positiveProbability = 1.0;
+                double negativeProbability = 1.0;
+                for (int item : list) {
+                    double positiveWordProbability = positiveList.getOrDefault(item, 0);
+                    positiveProbability *= ((positiveWordProbability + 1.0) / (positiveList.size() + wordCounter.size()));
+                    double negativeWordProbability = negativeList.getOrDefault(item, 0);
+                    negativeProbability *= ((negativeWordProbability + 1.0) / (negativeList.size() + wordCounter.size()));
                 }
-                average /= list.length;
-                if (average > 0.5) {
+                if(positiveProbability > negativeProbability){
                     System.out.println(1);
                 } else {
                     System.out.println(0);
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
